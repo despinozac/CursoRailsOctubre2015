774df6b845a8e279b371045fdabc05d2790d41b1
Carlos Torrealba <calrrox@gmail.com>
2015-10-23 09:36:23 -0430

    2er.- Creamos nuestra clase y migracion para company. No olviden corre la migracion con "rake db:migrate" *.- clase_2015_10_21

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009002912) do
+ActiveRecord::Schema.define(version: 20151023135941) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "line1"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20151009002912) do
   end
 
   add_index "addresses", ["person_id"], name: "index_addresses_on_person_id"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "rif"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
