4196b60220cddfd5c8cb37724c752bf32cae6c3b
Carlos Torrealba <calrrox@gmail.com>
2015-10-23 09:53:49 -0430

    4to.- Crearmos las caracteristicas minimas para mejenar nuestras compañias (CR*D). *.- clase_2015_10_21

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,36 @@
 class CompaniesController < ApplicationController
+
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(permit_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @company = Company.find(params[:id])
+
+    @company.destroy
+
+    redirect_to companies_path
+  end
+
+  private
+  def permit_params
+     params.require(:company).permit(:name, :rif)
+  end
 end
